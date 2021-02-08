ecdebf61ab8dde3f44500c41a68f37ebda4b9d9e
mehmethalis <mehmethaliscicek@gmail.com>
2021-02-08 18:07:58 +0300

    mongo db connection provided with mongoose

---
+++ server/Models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    userName: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('user', UserSchema);
--- server/app.js
+++ server/app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const {ApolloServer} = require('apollo-server-express');
 const {importSchema} = require('graphql-import');
 
 const resolvers = require('./graphql/resolvers/index');
 
 const server = new ApolloServer({
-    typeDefs: importSchema('./graphql/types/schema.graphql'),
+    typeDefs: importSchema('./graphql/schema.graphql'),
     resolvers
 });
+
+//db
+mongoose.connect(process.env.MONGO_DB_URI,{ useUnifiedTopology: true,useNewUrlParser: true } )
+    .then(()=>console.log('✔ MongoDB connected'))
+    .catch((error)=>console.log('✘ Could not connect to MongoDB. ----> ' + error))
 
 const app = express();
 server.applyMiddleware({app});
 
-app.listen({port: 4000}, () => {
-    console.log(`✔ Server ready at http://localhost:4000${server.graphqlPath}`)
+app.listen({port:process.env.PORT}, () => {
+    console.log(`✔ Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
 });
